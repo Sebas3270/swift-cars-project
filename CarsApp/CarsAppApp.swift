cc2f1cfabd74c7507ded1b6005fc370cb4c5ad9f
Sebastian <salvarezocampo@hotmail.com>
2023-11-09 20:33:00 -0600

    Initial Commit

---
+++ CarsApp/CarsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsAppApp.swift
+//  CarsApp
+//
+//  Created by Sebastián Alvarez on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CarsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
